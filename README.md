1f8ba3293214bc09fe1e2ad6dbf0ddbb6ae615a7
Mason Malone <mason@masonm.org>
2018-04-14 23:27:05 -0700

    Updates

---
+++ README.md
@@ -0,0 +1,41 @@
+# Overview
+
+[![Build Status](https://travis-ci.org/MasonM/wiremock-stub-mapping-tracker.svg?branch=master)](https://travis-ci.org/MasonM/wiremock-unused-stubs-extension)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.masonm/wiremock-stub-mapping-tracker/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.github.masonm/wiremock-stub-mapping-tracker)
+
+wiremock-stub-mapping-tracker is a set of [WireMock](http://wiremock.org) extensions for tracking which stub mappings have been used in a request, and easily delete unmatched mappings. This is useful in conjunction with [Record and Playback](http://wiremock.org/docs/record-playback-legacy/) for pruning generated stub mappings. Tracking is done by recording a list of stub mappings IDs that have been used to serve a request, and is independent of the request journal.
+
+# Building
+
+Run `gradle jar` to build the JAR without dependencies or `gradle fatJar` to build a standalone JAR. These will be placed in `build/libs/`.
+
+# Running
+
+Standalone server:
+```sh
+java -jar build/libs/wiremock-stub-mapping-tracker-0.1-standalone.jar
+```
+
+With WireMock standalone JAR:
+```sh
+wget -nc http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.14.0/wiremock-standalone-2.14.0.jar
+java \
+        -cp wiremock-standalone-2.14.0.jar:build/libs/wiremock-stub-mapping-tracker-0.1.jar \
+        com.github.tomakehurst.wiremock.standalone.WireMockServerRunner \
+        --extensions="com.github.masonm.StubMappingTrackerAdminExtension,com.github.masonm.StubMappingTrackerPostServeExtension"
+```
+
+Programmatically in Java:
+```java
+new WireMockServer(wireMockConfig()
+    .extensions("com.github.masonm.StubMappingTrackerAdminExtension", "com.github.masonm.StubMappingTrackerPostServeExtension"))
+```
+
+# Usage
+
+* `GET /mappings_tracker/matched` - Return all stub mappings that have matched a request
+* `GET /mappings_tracker/unmatched` - Return all stub mappings that haven't matched a request
+* `POST /mappings_tracker/reset` - Clear list of tracked stub mappings
+* `DELETE /mappings_tracker/unmatched` - Delete all stub mappings that haven't matched a request
+
+
--- test-script.sh
+++ test-script.sh
@@ -1,34 +1,68 @@
 #!/bin/bash
 
-PORT=8080
+VERSION=0.1
+WIREMOCK_BASE_URL=http://localhost:8080
+
+curl_tracker() { curl -s $WIREMOCK_BASE_URL/__admin/mappings_tracker/"$@"; }
+curl_mappings() { curl -s $WIREMOCK_BASE_URL/__admin/mappings "$@"; }
+curl_tracker_uuids() { curl_tracker "$@" | jq -c '[.[].id]'; }
+get_mapping_uuids() { curl_mappings | jq -c '[.mappings[].id]'; }
+add_mapping() { curl_mappings -d '{ "request": { "url": "'$1'" } }' | jq -c '.uuid'; }
+
+assert() {
+    if [ $2 ]; then
+        echo "[ PASS ] $1"
+    else
+        echo -e "[\e[31mFAIL\e[0m] $1: \"$2\""
+    fi
+}
+
+start_test() {
+    echo -e "\n$1:"
+    reset
+}
 
 reset() {
-	curl -s -X POST http://localhost:$PORT/__admin/mappings_tracker/reset > /dev/null
-	curl -s -X DELETE http://localhost:$PORT/__admin/mappings > /dev/null
-	curl -s -d '{ "request": { "method": "GET", "url": "/foo" }, "response": { "body": "FOO" } }' http://localhost:$PORT/__admin/mappings > /dev/null
-	curl -s -d '{ "request": { "method": "GET", "url": "/bar" }, "response": { "body": "FOO" } }' http://localhost:$PORT/__admin/mappings > /dev/null
+    curl_tracker reset -X POST > /dev/null
+    curl_mappings -X DELETE > /dev/null
 }
 
-results() {
-	echo -e "MATCHED:"; curl http://localhost:$PORT/__admin/mappings_tracker/matched
-	echo -e "UNMATCHED:"; curl http://localhost:$PORT/__admin/mappings_tracker/unmatched
-	curl -s -X DELETE http://localhost:$PORT/__admin/mappings_tracker/unmatched
-	echo -e "\n\nAFTER DELETE:"; curl http://localhost:$PORT/__admin/mappings
-	reset
-}
+echo "Launching Wiremock standalone"
+java -jar build/libs/wiremock-stub-mapping-tracker-$VERSION-standalone.jar &
+WIREMOCK_PID=$!
+trap "kill $WIREMOCK_PID" exit
+
+echo -n "Waiting for Wiremock to start up."
+until $(curl --output /dev/null --silent --head ${WIREMOCK_BASE_URL}/__admin); do
+    echo -n '.'
+    sleep 1
+done
 
 
-echo -e "Trying with no requests (should delete both):"
-results
+start_test "Creating 1 mapping and call DELETE /__admin/mappings_tracker/unmatched"
+ID1=$(add_mapping '/foo')
+assert "check mapping is unmatched" "$(curl_tracker_uuids unmatched) == [$ID1]"
+assert "check nothing is matched" "$(curl_tracker_uuids matched) == []"
+curl_tracker unmatched -X DELETE
+assert "check mapping was deleted" "$(get_mapping_uuids) == []"
+assert "check nothing is unmatched" "$(curl_tracker_uuids unmatched) == []"
 
-echo -e "\n\nTrying with one matching request (should delete bar):"
-curl -s http://localhost:$PORT/foo > /dev/null
-curl -s http://localhost:$PORT/baz > /dev/null
-results
+start_test "Creating 2 mappings, make one matching request, then call DELETE"
+ID1=$(add_mapping '/foo')
+ID2=$(add_mapping '/bar')
+curl -s $WIREMOCK_BASE_URL/foo > /dev/null
+assert "check first mapping is unmatched" "$(curl_tracker_uuids unmatched) == [$ID2]"
+assert "check second mapping is matched" "$(curl_tracker_uuids matched) == [$ID1]"
+curl_tracker unmatched -s -X DELETE
+assert "check second mappings was deleted" "$(get_mapping_uuids) == [$ID1]"
+assert "check nothing is unmatched" "$(curl_tracker_uuids unmatched) == []"
 
-echo -e "\n\nTrying with both requests matching (should not delete any):"
-
-curl -s http://localhost:$PORT/foo > /dev/null
-curl -s http://localhost:$PORT/baz > /dev/null
-curl -s http://localhost:$PORT/bar > /dev/null
-results
+start_test "Trying with both requests matching (should not delete any)"
+ID1=$(add_mapping '/foo')
+ID2=$(add_mapping '/bar')
+curl -s $WIREMOCK_BASE_URL/foo > /dev/null
+curl -s $WIREMOCK_BASE_URL/bar > /dev/null
+curl_tracker unmatched -s -X DELETE
+assert "check no mapping was deleted" "$(get_mapping_uuids) == [$ID2,$ID1]"
+assert "check nothing is unmatched" "$(curl_tracker_uuids unmatched) == []"
+assert "check both are matched" "$(curl_tracker_uuids matched) == [$ID2,$ID1]"
